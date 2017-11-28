a12e836fc2a8988cc94716ad355d5fca2ca262e9
fervandisseldorp <fervandisseldorp@gmail.com>
2017-11-28 23:14:28 +0100

    start ui and logic

---
+++ Assets/scripts/MonsterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour {
+	public Rigidbody rigidbody;
+	public int hp;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = transform.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
