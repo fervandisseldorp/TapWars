1bbdc1da7368623bcebc2af4860b59b61ef10b66
fervandisseldorp <fervandisseldorp@gmail.com>
2018-01-03 15:36:15 +0100

    basic collision, camera alphas, text overflow

--- Assets/scripts/MonsterController.cs
+++ Assets/scripts/MonsterController.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterController : MonoBehaviour {
-	public Rigidbody rigidbody;
 	public int hp;
 	public int speed;
 
 	// Use this for initialization
 	void Start () {
-		rigidbody = GetComponent<Rigidbody> ();
-		rigidbody.velocity = transform.right * speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate ( Vector3.right * Time.deltaTime * speed );
 	}
 }
