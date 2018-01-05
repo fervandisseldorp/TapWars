0f9c9bbe651f6eb2e45230887a6889819198c470
fervandisseldorp <fervandisseldorp@gmail.com>
2018-01-05 14:37:45 +0100

    more socket stuff

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SocketIO;
 
 public class GameController : MonoBehaviour {
 	public int lives;
+	//private SocketIOComponent socket;
+	public GameObject socketObject;
+
+
+	void awake(){
+		DontDestroyOnLoad (this.gameObject);
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log ("start in gamecontroller");
+		//GameObject go = GameObject.Find ("SocketIO");
+		SocketIOComponent socket = socketObject.GetComponent<SocketIOComponent>();
+
+		Debug.Log ("not destroying gameobject socket on change");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
---
+++ Assets/scripts/TestFindSocket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class TestFindSocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("trying to find controller");
+		GameObject controller = GameObject.Find("Controller");
+
+		//GameObject socketObject = controller.Find("SocketIO");
+
+
+		if (controller != null) {
+			Debug.Log ("found something named Controller");
+		}
+			
+	}
+
+}
