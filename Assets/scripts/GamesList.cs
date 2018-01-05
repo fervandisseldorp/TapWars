f70cbc3e6e3bf23a94abbf576c999eaaa7790b3b
fervandisseldorp <fervandisseldorp@gmail.com>
2018-01-05 13:13:15 +0100

    adding multiple scenes

---
+++ Assets/scripts/GamesList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamesList : MonoBehaviour {
+	//private ObjectPool buttonObjectPool;
+
+	int[] roomsList = {0,1,2,3};
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("looping and showing rooms");
+		for(int i=0; i<roomsList.Length; i++){
+
+			int roomNr = roomsList [i];
+
+			//GameObject newButton = buttonObjectPool.GetObject();
+			//newButton.transform.SetParent(contentPanel);
+
+			//SampleButton sampleButton = newButton.GetComponent<SampleButton>();
+			//sampleButton.Setup(item, this);
+
+		}
+	}
+
+
+
+
+
+}
---
+++ Assets/scripts/LobbyScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyScript : MonoBehaviour {
+
+
+	public void goToWaitingScreen(){
+		Debug.Log ("moving to waiting screen");
+
+		Application.LoadLevel ("WaitingForOpponent");
+	}
+}
